2b9b459440c2e7cc800337569392d5b186bce1f8
jaluik <cqzhengmouren@gmail.com>
2019-07-22 14:09:25 +0800

    更新正则边界测试

--- regTest.js
+++ regTest.js
@@ -22,6 +22,17 @@
 
 // let reg = /^([01][0-9])|(2[0-3]):[0-5][0-9]/g
 
-let reg = /[a-zA-Z]:\\([\w]+\\)+/g
-let str = 'F:\study\javascript\regex\regular expression.pdf F:\study\javascript\regex\ F:\study\javascript'
-console.log(str.match(reg))
+// let reg = /[a-zA-Z]:\\([\w]+\\)+/g
+// let str = 'F:\study\javascript\regex\regular expression.pdf F:\study\javascript\regex\ F:\study\javascript'
+// console.log(str.match(reg))
+
+// 匹配位置
+// 1.
+// let result = 'hello'.replace(/^|$/g, '#').replace(/^|$/g, '%')
+// console.log(result);
+// let result = 'I\nlove\njavascript'.replace(/^|$/gm, '#')
+// console.log(result)
+
+// 2.\b表示 \w与\W之间的边界
+// let result = '[JS] Lesson_01.mp4'.replace(/\B/g, '#')
+// console.log(result)
