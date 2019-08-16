63b05babefa973839eeeecbd44251a2e6b66d121
jaluik <cqzhengmouren@gmail.com>
2019-08-16 14:51:53 +0800

    更新自定义实现的promise函数

--- promise.js
+++ promise.js
@@ -13,17 +13,17 @@ function isFunction(fn){
 }
 
 const PENDING = 'PENDING'
-const FULFILED = 'FULFILED'
+const FULFILLED = 'FULFILLED'
 const REJECTED = 'REJECTED'
 
 class MyPromise{
     constructor(handle){
-        if(!isFunction(fn)){
+        if(!isFunction(handle)){
             throw new Error('传入的参数必须是一个函数')
         }
         this._status = PENDING
         this._value = undefined
-        this._fulfiledQueues = []
+        this._fulfilledQueues = []
         this._rejectedQueues = []
         try{
             handle(this._resolve.bind(this))
@@ -34,32 +34,89 @@ class MyPromise{
     }
     _resolve(val){
         if(this._status !== PENDING) return 
-        this._status = FULFILED
-        this._value = val
+        const run = ()=> {
+            this._status = FULFILLED
+            this._value = val
+            let cb;
+            while (cb = this._fulfilledQueues.shift()){
+                cb(val)
+            }
+        }
+        setTimeout( ()=> run(), 0)
     }
 
     _reject(err){
         if(this._status !== PENDING) return 
-        this._status = REJECTED
-        this._value = err
+        const run = () => {
+            this._status = REJECTED
+            this._value = err
+            let cb;
+            while(cb = this._rejectedQueues.shift()){
+                cb(err)
+            }
+        }
+        setTimeout( ()=> run(), 0)
     }
 
-    then(onFulfiled, onRejected){
+    then(onFulfilled, onRejected){
         const { _value, _status} = this
-        switch (_status){
-            case PENDING: 
-                this._fulfiledQueues.push(onFulfiled)
-                this._rejectedQueues.push(onRejected)
-                break
-            case FULFIELD: 
-                onFulfiled(_value)
-                break
-            case REJECTED:
-                onRejected(_value)
-                break
-        }
         return new MyPromise( (onFulfieldNext, onRejectedNext) => {
+            // 封装一个执行成功的函数
+            let fulfilled  = value => {
+                try {
+                    if(!isFunction(onFulfilled)){
+                        onFulfieldNext(value)
+                    }else{
+                        let res = onFulfilled(value)
+                        if(res instanceof MyPromise){
+                            res.then(onFulfieldNext, onRejectedNext)
+                        }else{
+                            onFulfieldNext(res)
+                        }
+                    }
+                } catch(err){
+                    onRejected(err)
+                }
+            }
+
+            let rejected = err => {
+                try {
+                    if(!isFunction(onRejected)){
+                        onRejectedNext(err)
+                    }else{
+                        let res = onRejected(err)
+                        if(res instanceof MyPromise){
+                            res.then(onFulfieldNext, onRejectedNext)
+                        }else{
+                            onFulfieldNext(res)
+                        }
+                    }
+
+                }catch(error){
+                    onRejectedNext(error)
+                }
+            }
+            switch (_status){
+                case PENDING: 
+                    this._fulfilledQueues.push(onFulfilled)
+                    this._rejectedQueues.push(onRejected)
+                    break
+                case FULFIELD: 
+                    fulfilled(_value)
+                    break
+                case REJECTED:
+                    rejected(_value)
+                    break
+            }
             
         })
     }
 }
+
+let a = new MyPromise((res, rej) => {
+    setTimeout( ()=>{
+        res('hahaha ')
+    },2000)
+}).then((val)=> {
+    console.log(val)
+})
